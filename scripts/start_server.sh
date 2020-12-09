018988e65facdc727741820472b279c7a9572481
Joshua Kopman <joshkopman@gmail.com>
2020-12-08 21:41:40 -0500

    cd location

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 docker stop $(docker ps -aq) || echo 'no containers to stop'
 docker rm -f $(docker ps -aq) || echo 'no containers to remove'
 docker rmi -f $(docker images -aq) || echo 'no images to remove'
-cd .. && docker build -t joshuakopman/pandemicpictures . || echo 'no images to build'
-cd .. && docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures || echo 'no containers to run'
+cd /home/ec2-user/ && docker build -t joshuakopman/pandemicpictures . || echo 'no images to build'
+cd /home/ec2-user/ && docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures || echo 'no containers to run'
