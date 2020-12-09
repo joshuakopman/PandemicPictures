700081c360010a5a8866b19c2988834e8658b0a6
Joshua Kopman <joshkopman@gmail.com>
2020-12-08 21:35:58 -0500

    updating start script

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,6 +1,5 @@
 docker stop $(docker ps -aq) || echo 'no containers to stop'
 docker rm -f $(docker ps -aq) || echo 'no containers to remove'
 docker rmi -f $(docker images -aq) || echo 'no images to remove'
-cd ..
-docker build -t joshuakopman/pandemicpictures .  || echo 'no images to build'
-docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures || echo 'no containers to run'
+cd .. && docker build -t joshuakopman/pandemicpictures . || echo 'no images to build'
+cd .. && docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures || echo 'no containers to run'
