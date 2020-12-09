bf9b1e40bf8a896a500855cbeecd1e704b7be361
Joshua Kopman <joshkopman@gmail.com>
2020-12-08 22:12:46 -0500

    splitting out bild script

---
+++ deployScripts/install.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+cd /home/ec2-user/aws-code-pandemic-pictures && docker build -t joshuakopman/pandemicpictures .
--- deployScripts/start_server.sh
+++ deployScripts/start_server.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-cd /home/ec2-user/aws-code-pandemic-pictures && docker build -t joshuakopman/pandemicpictures .
 cd /home/ec2-user/aws-code-pandemic-pictures && docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures
