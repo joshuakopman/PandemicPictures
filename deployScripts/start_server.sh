78dbfeb0f5c05c1ce74569dda582acbfe0afd6c5
Joshua Kopman <joshkopman@gmail.com>
2020-12-08 23:17:25 -0500

    setting up volume to read/write movie ratings from server

--- deployScripts/start_server.sh
+++ deployScripts/start_server.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cd /home/ec2-user/aws-code-pandemic-pictures && docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures
+cd /home/ec2-user/aws-code-pandemic-pictures && docker run -d -p 80:3000 -p 3000:3000  -v /home/ec2-user/movieRatingsDataStore/movies.json:/mocks/movies.json joshuakopman/pandemicpictures
