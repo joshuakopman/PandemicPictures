a614300086fee62aee484424e7f02d6ebf492f69
Joshua Kopman <joshkopman@gmail.com>
2020-12-09 10:51:37 -0500

    disabling proxy

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = 3000;
 const NomNomProvider = new NomineeProvider();
 const wsServer = new ws.Server({ noServer: true });
 const handlebars = hbsHelpers(exphbs);
-const ddos = new Ddos({burst:10, limit:15, maxexpiry:300})
+const ddos = new Ddos({burst:10, limit:15, maxexpiry:300, trustProxy: false})
 
 app.use(compression());
 app.use(ddos.express);
