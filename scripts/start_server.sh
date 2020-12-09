d1e53e49fc8ba5acb9f2c9d3d1dffb941fd5b69c
Joshua Kopman <joshkopman@gmail.com>
2020-12-08 21:29:03 -0500

    run as ec2-user

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
-sudo docker stop $(docker ps -aq) || echo 'no containers to stop'
-sudo docker rm -f $(docker ps -aq) || echo 'no containers to remove'
-sudo docker rmi -f $(docker images -aq) || echo 'no images to remove'
+docker stop $(docker ps -aq) || echo 'no containers to stop'
+docker rm -f $(docker ps -aq) || echo 'no containers to remove'
+docker rmi -f $(docker images -aq) || echo 'no images to remove'
 cd ..
-sudo docker build -t joshuakopman/pandemicpictures .  || echo 'no images to build'
-sudo docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures || echo 'no containers to run'
+docker build -t joshuakopman/pandemicpictures .  || echo 'no images to build'
+docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures || echo 'no containers to run'
