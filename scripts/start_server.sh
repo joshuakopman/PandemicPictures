2d6e443cbabbef538b6a5083d15ef21e5b6948bb
Joshua Kopman <joshkopman@gmail.com>
2020-12-08 21:43:31 -0500

    whatever let it fail

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -3,5 +3,5 @@
 docker stop $(docker ps -aq) || echo 'no containers to stop'
 docker rm -f $(docker ps -aq) || echo 'no containers to remove'
 docker rmi -f $(docker images -aq) || echo 'no images to remove'
-cd /home/ec2-user/ && docker build -t joshuakopman/pandemicpictures . || echo 'no images to build'
-cd /home/ec2-user/ && docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures || echo 'no containers to run'
+cd /home/ec2-user/aws-code-pandemic-pictures && docker build -t joshuakopman/pandemicpictures
+cd /home/ec2-user/aws-code-pandemic-pictures && docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures
