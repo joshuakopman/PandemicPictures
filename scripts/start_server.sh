9b4355c0af2e78979fac2d0c57701dbbe3a55822
Joshua Kopman <joshkopman@gmail.com>
2020-12-08 21:48:07 -0500

    finally. it works.

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-docker stop $(docker ps -aq) || echo 'no containers to stop'
-docker rm -f $(docker ps -aq) || echo 'no containers to remove'
-docker rmi -f $(docker images -aq) || echo 'no images to remove'
 cd /home/ec2-user/aws-code-pandemic-pictures && docker build -t joshuakopman/pandemicpictures .
 cd /home/ec2-user/aws-code-pandemic-pictures && docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures
--- scripts/stop_server.sh
+++ scripts/stop_server.sh
@@ -1,1 +1,5 @@
+#!/bin/bash
+
 docker stop $(docker ps -aq) || echo 'no containers to stop'
+docker rm -f $(docker ps -aq) || echo 'no containers to remove'
+docker rmi -f $(docker images -aq) || echo 'no images to remove'
