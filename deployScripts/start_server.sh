11818efeb87ee3f3ed3a0c1736488116282da047
Joshua Kopman <joshkopman@gmail.com>
2020-12-08 20:25:32 -0500

    adding scripts

---
+++ deployScripts/start_server.sh
@@ -0,0 +1,5 @@
+docker stop $(docker ps -aq)
+docker rm -f $(docker ps -aq)
+docker rmi -f $(docker images -aq)
+docker build -t joshuakopman/pandemicpictures .
+docker run  -p 80:3000 -p 3000:3000 -d joshuakopman/pandemicpictures
