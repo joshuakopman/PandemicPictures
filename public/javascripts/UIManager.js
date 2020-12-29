ee8d15b32ff1b2bb21135396bac1df54c3e05dbb
Joshua Kopman <joshkopman@gmail.com>
2020-12-28 23:48:10 -0500

    filters showing on admin/edit ciew

--- public/javascripts/UIManager.js
+++ public/javascripts/UIManager.js
@@ -62,6 +62,9 @@ class UIManager {
                 if (allMoviesLoaded) {
                     self.uiEventListenerManager.addRandomMovieClickListener(movieData);
                     self.uiEventListenerManager.addChevronClickListeners(imdbData);
+                    
+                    document.querySelector('#filtersNav').style.display = 'block';
+
                     if (window.location.href.includes('edit')) {
                         self.uiEventListenerManager.addInputClickListeners(movieData.MoviesList);
                     } else {
@@ -74,7 +77,6 @@ class UIManager {
                         });
 
                         document.body.classList.add("read-only");
-                        document.querySelector('#filtersNav').style.display = 'block';
                     }
                     if (window.location.hash) {
                         document.getElementById(window.location.hash.replace("#", "")).scrollIntoView();
