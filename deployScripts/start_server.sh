de8b7cfdeb7c17b458d0e811bf1490c1e37d696b
Joshua Kopman <joshkopman@gmail.com>
2021-01-31 15:51:46 -0500

    trying to mount directory versus each file

--- deployScripts/start_server.sh
+++ deployScripts/start_server.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-cd /home/ec2-user/aws-code-pandemic-pictures && docker run -d -p 80:3000 -p 3000:3000 \
--v /home/ec2-user/movieRatingsDataStore/movies_Josh_Alicia.json:/mocks/movies_Josh_Alicia.json \
--v /home/ec2-user/movieRatingsDataStore/movies_Zak_Neas.json:/mocks/movies_Zak_Neas.json \
--v /home/ec2-user/movieRatingsDataStore/movies_Steve_Allie.json:/mocks/movies_Steve_Allie.json \
--v /home/ec2-user/movieRatingsDataStore/movies_Marc_Joanne.json:/mocks/movies_Marc_Joanne.json \
-joshuakopman/pandemicpictures
+cd /home/ec2-user/aws-code-pandemic-pictures && docker run -d -p 80:3000 -p 3000:3000 -v /home/ec2-user/movieRatingsDataStore/:/mocks/ joshuakopman/pandemicpictures
