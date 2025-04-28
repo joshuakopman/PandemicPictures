04729b65ea110706feb637424de6ee5daa8d3b08
Joshua Kopman <joshkopman@gmail.com>
2025-04-27 23:31:47 -0400

    adding line to copy latest imdb json over to container

--- deployScripts/start_server.sh
+++ deployScripts/start_server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 cd /home/ec2-user/aws-code-pandemic-pictures && docker run -d -p 80:3000 -p 3000:3000 -v /home/ec2-user/movieRatingsDataStore/:/mocks/ joshuakopman/pandemicpictures
-
-cd /home/ec2-user/movieRatingsDataStore/ && cp * ../
+cd /home/ec2-user/aws-code-pandemic-pictures/mocks/ && cp ./imdb.json ../../movieRatingsDataStore/imdb.json 
+cd /home/ec2-user/movieRatingsDataStore/ && cp * ../ 
