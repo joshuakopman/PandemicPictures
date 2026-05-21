b8b0ca097515c3f76d6ed1f8bd562dd551fbb0c3
joshuakopman <joshkopman@gmail.com>
2026-05-21 12:49:29 -0400

    Ignore backup directories during deployment copy

--- deployScripts/start_server.sh
+++ deployScripts/start_server.sh
@@ -2,4 +2,4 @@
 
 cd /home/ec2-user/aws-code-pandemic-pictures && docker run -d -p 80:3000 -p 3000:3000 -v /home/ec2-user/movieRatingsDataStore/:/mocks/ joshuakopman/pandemicpictures
 cd /home/ec2-user/aws-code-pandemic-pictures/mocks/ && cp ./imdb.json ../../movieRatingsDataStore/imdb.json 
-cd /home/ec2-user/movieRatingsDataStore/ && cp * ../ 
+cd /home/ec2-user/movieRatingsDataStore/ && find . -maxdepth 1 -type f -exec cp {} ../ \;
